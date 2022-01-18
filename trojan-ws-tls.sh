68b0db14d8afd3396c14493c7ad71df740e54200
nevie <emaitian@gmail.com>
2022-01-18 23:06:18 +0800

    Update trojan-ws-tls.sh

--- trojan-ws-tls.sh
+++ trojan-ws-tls.sh
@@ -1,1 +1,6 @@
-123
+#!/bin/bash
+wget "https://raw.githubusercontent.com/emaitian/proxy/main/web.sh"
+wget "https://raw.githubusercontent.com/emaitian/inbox/main/trojan-go.sh"
+chmod +x *.sh
+bash web.sh
+bash trojan-ws-tls.sh
