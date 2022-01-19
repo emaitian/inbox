728ee7c14c5fb7f7894519c7cf06958011f3c2c5
nevie <emaitian@gmail.com>
2022-01-19 20:28:41 +0800

    Delete vless-tcp-xtls.sh

--- vless-tcp-xtls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget "https://raw.githubusercontent.com/emaitian/proxy/main/web.sh"
-wget "https://raw.githubusercontent.com/emaitian/inbox/main/xray.sh"
-chmod +x *.sh
-bash web.sh
-bash xray.sh
