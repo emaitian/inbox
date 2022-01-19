aec8642d5f00c00ae5c9394514f6c078ee86229b
nevie <emaitian@gmail.com>
2022-01-19 20:28:31 +0800

    Delete trojan-ws-tls.sh

--- trojan-ws-tls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget "https://raw.githubusercontent.com/emaitian/proxy/main/web.sh"
-wget "https://raw.githubusercontent.com/emaitian/inbox/main/trojan-go.sh"
-chmod +x *.sh
-bash web.sh
-bash trojan-ws-tls.sh
