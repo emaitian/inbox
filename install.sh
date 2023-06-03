7c7b32e74f69abd62b4d5a5f2482680f91a7897c
nevie <emaitian@gmail.com>
2023-06-03 19:39:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -120,12 +120,41 @@ cat > /usr/local/etc/xray/config.json << EOF
     "access": "/var/log/xray/access.log",
     "error": "/var/log/xray/error.log"
   },
+  
   "dns": {
     "servers": [
       "https+local://1.1.1.1/dns-query",
       "localhost"
     ]
   },
+
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "block"
+      },
+	  
+      {
+        "type": "field",
+        "ip": ["geoip:cn"],
+        "outboundTag": "block"
+      },
+	  
+      {
+        "type": "field",
+        "domain": [
+          "geosite:category-ads-all"
+        ],
+        "outboundTag": "block"
+      }
+    ]
+  },
+
   "inbounds": [
     {
       "port": 443,
@@ -134,9 +163,9 @@ cat > /usr/local/etc/xray/config.json << EOF
         "clients": [
           {
             "id": "7bb4a015-48f2-46ab-ab24-585f69c6a034",
-            "flow": "xtls-rprx-direct",
+            "flow": "xtls-rprx-vision",
             "level": 0,
-            "email": "admin@yourdomain.com"
+            "email": "emaitian@gmail.com"
           }
         ],
         "decryption": "none",
@@ -148,11 +177,9 @@ cat > /usr/local/etc/xray/config.json << EOF
       },
       "streamSettings": {
         "network": "tcp",
-        "security": "xtls",
-        "xtlsSettings": {
-          "allowInsecure": false,
-          "minVersion": "1.2",
-          "alpn": ["http/1.1"],
+        "security": "tls",
+        "tlsSettings": {
+          "alpn": "http/1.1",
           "certificates": [
             {
               "certificateFile": "/home/cert/fullchain.crt",
@@ -163,98 +190,25 @@ cat > /usr/local/etc/xray/config.json << EOF
       }
     }
   ],
+
   "outbounds": [
     {
       "tag": "direct",
       "protocol": "freedom"
+    },
+
+    {
+      "tag": "block",
+      "protocol": "blackhole"
     }
   ]
 }
 EOF
-systemctl stop xray
-echo
-echo -e "\033[49;37;7m install trojan-go \033[0m"
-echo -----------------------------------
-mkdir -p /etc/trojan/bin/
-wget --no-check-certificate -O /etc/trojan/bin/trojan-go-linux-amd64.zip "https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip"
-apt install unzip -y
-unzip -o -d /etc/trojan/bin /etc/trojan/bin/trojan-go-linux-amd64.zip
-echo
-#
-echo
-echo -e "\033[49;37;7m trojan-go config \033[0m"
-echo -----------------------------------
-mkdir -p /etc/trojan/conf
-cat > /etc/trojan/conf/server.json << EOF
-{
-    "run_type": "server", 
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "remote_addr": "127.0.0.1",
-    "remote_port": 80,
-    "log_level": 1,
-    "log_file": "",
-    "password": ["7bb4a015-48f2-46ab-ab24-585f69c6a034"],
-    "disable_http_check": false,
-    "udp_timeout": 60,
-    "ssl": {
-        "verify": true,
-        "verify_hostname": true, 
-        "cert": "/home/cert/fullchain.crt",
-        "key": "/home/cert/nevie.buzz.key",
-        "key_password": "",
-        "cipher": "",
-        "curves": "",
-        "prefer_server_cipher": false,
-        "sni": "nevie.buzz",
-        "alpn": [
-            "http/1.1"
-        ],
-        "session_ticket": true,
-        "reuse_session": true,
-        "plain_http_response": "",
-        "fallback_addr": "127.0.0.1",
-        "fallback_port": 80,
-        "fingerprint": ""
-    },
-    "websocket": {
-        "enabled": true,
-        "path": "/random",
-        "host": "nevie.buzz"
-    }
-}
-EOF
-echo
-#
-echo -e "\033[49;37;7m create trojan.service \033[0m"
-echo -----------------------------------
-cat >/etc/systemd/system/trojan.service << EOF
-[Unit]
-Description=trojan
-Documentation=https://github.com/p4gefau1t/trojan-go
-After=network.target
-[Service]
-Type=simple
-StandardError=journal
-PIDFile=/usr/src/trojan/trojan/trojan.pid
-ExecStart=/etc/trojan/bin/trojan-go -config /etc/trojan/conf/server.json
-ExecReload=
-ExecStop=/etc/trojan/bin/trojan-go
-LimitNOFILE=51200
-Restart=on-failure
-RestartSec=1s
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
+systemctl restart xray
 echo
 echo -e "\033[49;37;7m status \033[0m"
 echo -----------------------------------
 systemctl status nginx
-echo
-echo -e "\033[49;37;7m trojan-go \033[0m"
-echo -----------------------------------
-systemctl status trojan.service
 echo
 echo -e "\033[49;37;7m xray \033[0m"
 echo -----------------------------------
